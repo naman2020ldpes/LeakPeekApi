ebff5561f940495a6546045da0805e0c5b3fddc4
namanshajain <142691717+namanshajain@users.noreply.github.com>
2025-04-25 16:40:17 +0530

    to fix all url and provide end point

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask 
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app) 
 from controller import *  # package
 if __name__ == "__main__":
     app.run(debug=True)
